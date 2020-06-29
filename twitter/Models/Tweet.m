edd9a190412c99f4ef767ca8f043471f2be732fe
Ria Vora <riavora@fb.com>
2020-06-29 13:50:30 -0700

    created data models for tweets and users!

---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,59 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Ria Vora on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        
+        NSDictionary *originalTweet = dictionary[@"retweeted_status"];
+        if (originalTweet != nil) {
+            NSDictionary *userDictionary = dictionary[@"user"];
+            self.retweetedByUser = [[User alloc] initWithDictionary:userDictionary];
+            
+            dictionary = originalTweet;
+        }
+        
+        self.idStr = dictionary[@"id_str"];
+        self.text = dictionary[@"text"];
+        self.favoriteCount = [dictionary[@"favorite_count"] intValue];
+        self.favorited = dictionary[@"favorited"];
+        self.retweetCount = [dictionary[@"retweet_count"] intValue];
+        self.retweeted = [dictionary[@"retweeted"] boolValue];
+        
+        NSDictionary *user = dictionary[@"user"];
+        self.user = [[User alloc] initWithDictionary:user];
+        self.createdAt = [self getFormattedString:dictionary];
+
+    }
+    return self;
+}
+
+- (NSString *)getFormattedString:(NSDictionary *)dictionary {
+    NSString *createdAtOriginalString = dictionary[@"created_at"];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+    NSDate *date = [formatter dateFromString:createdAtOriginalString];
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    formatter.timeStyle = NSDateFormatterNoStyle;
+    return [formatter stringFromDate:date];
+}
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries {
+    NSMutableArray *tweets = [NSMutableArray array];
+    for (NSDictionary *dictionary in dictionaries) {
+        Tweet *tweet = [[Tweet alloc] initWithDictionary:dictionary];
+        [tweets addObject:tweet];
+    }
+    return tweets;
+}
+
+@end
